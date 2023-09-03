7535d45d7ddd70a500c1b75e0dba77ce50f3239f
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2023-09-03 13:41:35 +0300

    setup routes and add homePage view
    - Setup routes at App comp.
    - Add HomePage view to view all Homepage content  AKA Customers list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+// helpers
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Nested components
+import HomePage from "./Views/HomePage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Views/HomePage.tsx
@@ -0,0 +1,10 @@
+// Helpers
+import { FC } from "react";
+
+// Nested Components
+
+const HomePage: FC = () => {
+  return <div>home page</div>;
+};
+
+export default HomePage;
