93a0afa9336e42c0589214ca0c743f7eac228279
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2023-09-03 14:43:15 +0300

    setup Orders page route
    - Add Orders Page view to view each customer orders
    - Orders Page is a viewer to view all orders components
    -  Setup orders route at App comp. with
    dynamic id for each customer

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Nested components
 import HomePage from "./Views/HomePage";
+import OrdersPage from "./Views/OrdersPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="orders/:current_customer_id" element={<OrdersPage />} />
       </Routes>
     </Router>
   );
---
+++ src/Views/OrdersPage.tsx
@@ -0,0 +1,10 @@
+// Helpers
+import { FC } from "react";
+
+// Nested Components
+
+const OrdersPage: FC = () => {
+  return <div>OrdersPage</div>;
+};
+
+export default OrdersPage;
