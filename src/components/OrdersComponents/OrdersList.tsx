746944f4bddfe78086a09f56cb7c551f94d314a4
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2023-09-03 18:41:34 +0300

    consume single customer orders
    - Setup consuming customerOrdersList at OrdersList Comp. by:
    -- Get current customer id form url with useParam hook
    -- Get setCustomerOrders method from OrdersContext
    -- Validate current customer Id then pass its value to setCustomerOrders
    -- Consume currentCustomerOrders
    -- Display currentCustomerOrders by looping over & pass data to singleOrder comp.

--- src/components/OrdersComponents/OrdersList.tsx
+++ src/components/OrdersComponents/OrdersList.tsx
@@ -1,15 +1,25 @@
 // helpers
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { useOrdersContext } from "../../utils/Hooks";
 import { OrderInterface } from "../../utils/Interface";
+import { useParams } from "react-router-dom";
+
+// Nested Components
 import SingleOrder from "./SingleOrder";
 
 const OrdersList: FC = () => {
-  const { ordersList } = useOrdersContext();
+  // get current customer orders list & setCustomerOrders from context
+  const { customerOrdersList, setCustomerOrders } = useOrdersContext();
+  // get current user id
+  const { current_customer_id } = useParams();
 
+  useEffect(() => {
+    // check incase no current customer id before setting current customer orders at the store
+    if (current_customer_id) setCustomerOrders(parseInt(current_customer_id));
+  }, [current_customer_id, setCustomerOrders]);
   return (
     <section>
-      {ordersList.map((order: OrderInterface) => {
+      {customerOrdersList.map((order: OrderInterface) => {
         return <SingleOrder key={order.id} orderData={order} />;
       })}
     </section>
