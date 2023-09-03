dd04cdb9be0279b5b1e905b2753f424f725a3e62
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2023-09-03 19:10:09 +0300

    create AddCustomerContainer
    - Add AddCustomerComponents folder to contain all AddCustomer components files
    - Add AddCustomerContainer comp. to contain all AddCustomer components
    - Import & use AddCustomerContainer in AddCustomerPage view

--- src/Views/AddCustomerPage.tsx
+++ src/Views/AddCustomerPage.tsx
@@ -2,8 +2,15 @@
 import { FC } from "react";
 import { CustomersProvider } from "../store/CustomerStore";
 
+// Nested Components
+import AddCustomerContainer from "../components/CustomersComponents/AddCustomerComponents/AddCustomerContainer";
+
 const AddCustomerPage: FC = () => {
-  return <CustomersProvider>AddCustomerPage</CustomersProvider>;
+  return (
+    <CustomersProvider>
+      <AddCustomerContainer />
+    </CustomersProvider>
+  );
 };
 
 export default AddCustomerPage;
---
+++ src/components/CustomersComponents/AddCustomerComponents/AddCustomerContainer.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const AddCustomerContainer: FC = () => {
+  return <div>AddCustomerContainer</div>;
+};
+
+export default AddCustomerContainer;
